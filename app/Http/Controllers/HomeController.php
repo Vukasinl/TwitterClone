479b56e514f268f29f5feff04e1b2cfb5c5e1093
Vukasinl <lalosevic.vukasin@gmail.com>
2021-02-27 23:08:13 +0100

    Table migrations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd(request()->user()->tweetsFromFollowing->pluck('body'));
+
         return view('home');
     }
 }

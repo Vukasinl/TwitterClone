0eb504e62aaed59230516c6b8e36cec8ab5b4cfe
Vukasinl <lalosevic.vukasin@gmail.com>
2021-02-27 23:42:09 +0100

    Add tailwind

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(request()->user()->tweetsFromFollowing->pluck('body'));
-
         return view('home');
     }
 }

cdc117ff097b28f78b84dce7a610587f144477f8
Vukasinl <lalosevic.vukasin@gmail.com>
2021-03-09 05:05:58 +0100

    Show retweets

--- app/Http/Resources/TweetResource.php
+++ app/Http/Resources/TweetResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+// use App\Http\Resources\TweetResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TweetResource extends JsonResource
@@ -15,10 +16,12 @@ class TweetResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'body'       => $this->body,
-            'user'       => new UserResource($this->user),
-            'created_at' => $this->created_at->timestamp
+            'id'             => $this->id,
+            'body'           => $this->body,
+            'type'           => $this->type,
+            'original_tweet' => new TweetResource($this->originalTweet),
+            'user'           => new UserResource($this->user),
+            'created_at'     => $this->created_at->timestamp
         ];
     }
 }
---
+++ resources/js/components/tweets/variants/AppTweetVariantTweet.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex w-full">
+    <div class="mr-3">
+      <img
+        class="w-12 rounded-full"
+        :src="tweet.user.avatar"
+        :alt="tweet.user.name"
+      />
+    </div>
+    <div>
+      <app-tweet-username :user="tweet.user" />
+      <p class="text-gray-300 whitespace-pre-wrap">{{ tweet.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tweet: {
+      required: true,
+      type: Object
+    }
+  }
+};
+</script>

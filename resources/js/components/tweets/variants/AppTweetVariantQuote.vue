09c3bc3422a16e47fd81c4f5b92076ab1af93654
Vukasinl <lalosevic.vukasin@gmail.com>
2021-03-11 21:52:51 +0100

    Display quoted tweets

--- resources/js/components/tweets/variants/AppTweetVariantQuote.vue
+++ resources/js/components/tweets/variants/AppTweetVariantQuote.vue
@@ -9,7 +9,13 @@
     </div>
     <div class="flex-grow">
       <app-tweet-username :user="tweet.user" />
+
       <p class="text-gray-300 whitespace-pre-wrap">{{ tweet.body }}</p>
+
+      <app-tweet
+        class="border border-gray-700 rounded-lg mt-4"
+        :tweet="tweet.original_tweet"
+      />
     </div>
   </div>
 </template>

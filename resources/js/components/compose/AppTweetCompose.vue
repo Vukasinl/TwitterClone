a9f1818569dc037b549bfa023545553ed5be1a71
Vukasinl <lalosevic.vukasin@gmail.com>
2021-03-15 01:08:37 +0100

    Tweet limit indicator

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -13,7 +13,13 @@
 
         </div>
 
-        <div>
+        <div class="flex items-center justify-end">
+          <div>
+            <app-tweet-compose-limit
+              class="mr-2"
+              :body="form.body"
+            />
+          </div>
           <button
             type="submit"
             class="bg-blue-500 rounded-full text-gray-300 text-center px-4 py-3 font-bold leading-none"
